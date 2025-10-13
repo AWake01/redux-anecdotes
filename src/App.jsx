f557fb31f524aee0407e568ae4a0ef0647388efe
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-10-13 15:51:08 +0100

    Cheanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import { doVote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state).sort((a,b) => b.votes - a.votes)
-  const dispatch = useDispatch()
-
   return (
     <div>
       <AnecdoteList/>
